a7ff7415aff998569a5f704548f3a1bcb7d46fa3
Justin Proulx (aka Sn0wCh1ld) <jawproulx2001@icloud.com>
2017-11-07 10:29:16 -0500

    new tableviewcontroller and associated files

---
+++ MobilePortail/MobilePortail/ResultsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsTableViewController.h
+//  MobilePortail
+//
+//  Created by Justin Proulx on 2017-11-07.
+//  Copyright © 2017 Bricc Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsTableViewController : UITableViewController
+
+@end
